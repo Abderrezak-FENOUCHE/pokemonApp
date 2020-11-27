9b8e93ed4c6a21a5d067ccc3b9f907251071492a
Abderrezak FENOUCHE <abfenouche@gmail.com>
2020-11-27 11:51:25 +0100

    Réalisation du premier Tp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'pokemon',component:ListPokemonComponent},
+  {path:"pokemon/:id", component:DetailPokemonComponent},
+  {path:'',redirectTo:'pokemon', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
